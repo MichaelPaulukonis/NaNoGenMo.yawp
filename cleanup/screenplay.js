07f5d10ffcfe51db80de4280f2768e5efdcfb4b9
Michael Paulukonis <xraysmalevich@gmail.com>
2013-11-16 23:34:27 -0500

    output to JSON; notes towards fixing some regexes, and other issues.

--- cleanup/screenplay.js
+++ cleanup/screenplay.js
@@ -23,10 +23,16 @@ var processor = function(err, data) {
 
     // regexes to identify flush-left text (delete) and Characters
     // also.. who knows what
-    var lines = [], newlines = [], charnames = [], 
+    var lines = [], newlines = [], charnames = [],
 	tmp = "", character = "", dialogue = [],
 	flushleftRe = /^\b/, characterRe = /([A-Z]{2,}( [A-Z]*)?)/;
-    
+
+    var store = { characters: [],
+                  dialogue: [],
+                  chardialogue: [],
+                  directions: []
+                  };
+
     lines = data.trim().split("\n");
 
     lines.forEach(function(line) {
@@ -38,22 +44,25 @@ var processor = function(err, data) {
 	// blank line found
 	//   emit charname: dialogue
 	if (flushleftRe.exec(line)) return;
-	
-	debugger;
-	
+
 	line = line.trim();
+        // if (line.length == 0) return; // can't do this, because we use blank-lines as a trigger for storage
+        // ie, dialogue is over. awkard.
 
 	// save character name
+        // fails for THREE TOUGH KIDS
+        // needs to skip the "o.s." part of WITCH o.s.
+        // DOROTHY AND SCARECROW
+        // MUNCHKIN NO. 1
 	var crctr = characterRe.exec(line);
 	if(crctr) {
 	    character = crctr[0];
-	    //console.log('character: ' + character);
+            if (store.characters.indexOf(character) == -1) store.characters.push(character);
 	    return;
 	};
 
 	// if not blank line, store as dialogue
 	if (character && line.length > 0) {
-	    console.log(line);
 	    dialogue.push(line);
 	    return;
 	};
@@ -63,12 +72,25 @@ var processor = function(err, data) {
 	// this seems hackish?
 	// ALSO: we are ignore "stage directions" or whatever that setup stuff is.
 	// ALSO: some things slip through (see OZ)
-	if (character) newlines.push(character + ": " + dialogue.join(" "));
+        var lump;
+	if (character) {
+            lump = dialogue.join(" ");
+            store.dialogue.push(lump);
+            lump = character + ": " + lump;
+            store.chardialogue.push(lump);
+        } else {
+            // currently being discarded by flush-left directive
+            lump = dialogue.join(" ");
+            store.directions.push(lump);
+        }
+        newlines.push(lump);
+
 	character = "";
 	dialogue = [];
     });
 
     fs.writeFile(outfile, newlines.join("\n"));
+    fs.writeFile(outfile + ".json", JSON.stringify(store));
 
 };
 
